9b26ce248e72609be79a7b969e2f4db8f9a1e4ce
Jeffrey196 <jeffreyvh@ymail.com>
2019-01-22 08:54:56 -0600

    first init

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Configure body parsing for AJAX requests
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Add routes, both API and view
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
