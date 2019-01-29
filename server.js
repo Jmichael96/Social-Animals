995d189b8afd4ad3dce937aa2286139ac8496949
Short Stack <41342213+rchlblns@users.noreply.github.com>
2019-01-28 19:06:59 -0600

    yarn install petfinder api package and started routing to api

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const dotenv = require("dotenv").config();
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -18,3 +18,5 @@ app.use(routes);
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
+
+module.exports = dotenv;
