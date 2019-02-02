1bcfb706bb3134cf47d71db10feae02243cd8b04
Short Stack <41342213+rchlblns@users.noreply.github.com>
2019-02-01 20:12:01 -0600

    added markers and user geolocation to map

--- client/src/components/MapContainer/index.js
+++ client/src/components/MapContainer/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+import CurrentLocation from "../MapLocation/index";
 
 const mapstyles = {
   width: "100%",
@@ -8,17 +9,50 @@ const mapstyles = {
 
 
 export class MapContainer extends Component {
+
+  state = {
+    showingInfoWindow: false, // hides/shows the info window (hidden by default)
+    activeMarker: {},         // shows the active marker when clicked 
+    selectedPlace: {}         // shows the info window to the selected place upon a marker
+  };
+
+  handleMarkerClick = (props, marker, event) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  };
+
+  handleClose = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  };
+
   render() {
     return (
-      <Map
-        google={this.props.google}
-        zoom={14}
-        style={mapstyles}
-        initialCenter={{
-          lat: -1.2,
-          lng: 36
-        }}
-      />
+      <div>
+        <CurrentLocation
+          centerAroundCurrentLocation
+          google={this.props.google}
+        >
+          <Marker
+            onClick={this.handleMarkerClick}
+            name={"current location"}
+          />
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}
+            onClose={this.handleClose}
+          >
+            <h1>this.state.selectedPlace.name</h1>
+          </InfoWindow>
+        </CurrentLocation>
+      </div>
     );
   }
 }
