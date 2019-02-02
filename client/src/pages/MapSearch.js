322b44d57887e0a63a9894dd11f7f80c31fb6621
Short Stack <41342213+rchlblns@users.noreply.github.com>
2019-02-01 21:46:03 -0600

    search functionality added in Google Maps

--- client/src/pages/MapSearch.js
+++ client/src/pages/MapSearch.js
@@ -2,16 +2,49 @@ import React, { Component } from "react";
 import { MDBCol, MDBFormInline, MDBIcon, MDBBtn } from "mdbreact";
 import MapSearchBar from "../components/MapSearchBar/index"
 import MapContainer from "../components/MapContainer/index";
-
+import axios from "axios";
 
 class MapSearch extends Component {
+
+    constructor(props) {
+        super(props);
+        state = {
+            query: " ",
+            searchResults: []
+        }
+    }
+
+    search = () => {
+        const BASE_URL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input="
+        fetch(`${BASE_URL}${this.state.query}${process.env.REACT_APP_GOOGLE_APIKEY}`)
+            .then(res => Response.json())
+            .then(json => {
+                let { searchResults } = json;
+                this.setState({ items });
+                console.log(json);
+            })
+    }
     render() {
         return (
             <div>
-            <h1>Find pet related products and services here!</h1>
+                <h1>Find pet related products and services here!</h1>
                 <MapContainer />
-                <MapSearchBar />
+                <MapSearchBar
+                    onChange={event => this.setState({ query: event.target.value })}
+                    onClick={event => {
+                        if (event) {
+                            this.search();
+                        }
+                    }} />
+                <MDBRow>
+                    <MDBCol>
+                        <ul>
+                            {this.state.results.map(result => <li>{result.name}</li>)};
+                        </ul>
+                    </MDBCol>
+                </MDBRow>
             </div>
+
         );
     };
 };
