0a892684b8cc3c77901837d95bc31a32ddfb4d00
Jeffrey196 <jeffreyvh@ymail.com>
2019-02-03 20:57:57 -0600

    updated createpost page with a fancy form

--- client/src/components/CreatePost/index.js
+++ client/src/components/CreatePost/index.js
@@ -1,10 +1,11 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import axios from "axios";
 import { Redirect } from 'react-router-dom'
+import { MDBContainer, MDBBtn, MDBInput, MDBIcon } from 'mdbreact';
+import "./style.css";
 
-class CreatePost extends Component{
-
-    constructor(){
+class CreatePost extends Component {
+    constructor() {
         super()
         this.state = {
             title: "",
@@ -15,15 +16,17 @@ class CreatePost extends Component{
             redirectTo: null
         }
         this.handleSubmit = this.handleSubmit.bind(this)
-		this.handleChange = this.handleChange.bind(this)
+        this.handleChange = this.handleChange.bind(this)
     }
-
+    getPickerValue = value => {
+        console.log(value);
+    };
     handleChange(event) {
-		this.setState({
-			[event.target.name]: event.target.value
-		})
+        this.setState({
+            [event.target.name]: event.target.value
+        })
     }
-    handleSubmit(event){
+    handleSubmit(event) {
         event.preventDefault();
         axios.post('/api/create-post', {
             title: this.state.title,
@@ -33,78 +36,86 @@ class CreatePost extends Component{
             date: this.state.date,
 
         })
-        .then(response => {
-            console.log(response)
-            if (!response.data.errmsg) {
-                console.log('youre good')
-                this.setState({
-                    redirectTo: '/'
-                })
-            } else {
-                console.log('duplicate');
-            }
-        })
+            .then(response => {
+                console.log(response)
+                if (!response.data.errmsg) {
+                    console.log('youre good')
+                    this.setState({
+                        redirectTo: '/'
+                    })
+                } else {
+                    console.log('duplicate');
+                }
+            })
     }
     render() {
         if (this.state.redirectTo) {
-			return <Redirect to={{ pathname: this.state.redirectTo }} />
-		}
+            return <Redirect to={{ pathname: this.state.redirectTo }} />
+        }
         return (
             <div>
-                <form>
-                    <div className="form-group">
-                        <label htmlFor="title">Title</label>
-                        <input 
-                        type="text" 
-                        name="title" 
-                        value={this.state.title}
-                        onChange={this.handleChange}
-                        className="form-control form-control-lg" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="authorName">Your Name</label>
-                        <input 
-                        type="text" 
-                        name="authorName" 
-                        value={this.state.authorName} 
-                        onChange={this.handleChange}
-                        className="form-control form-control-lg" />
-                    </div>
-                </form>
-                <form>
-                    <div className="form-group">
-                        <label htmlFor="content">
-                            Summary
-                    </label>
-                        <textarea 
-                            type="text" 
-                            name="content"
-                            className="form-control"
-                            value={this.state.content}
+                <MDBContainer id="form">
+                    <form>
+                        <MDBInput
+                            label="Title"
+                            icon="sort-alpha-down"
+                            group
+                            type="text"
+                            validate
+                            error="wrong"
+                            success="right"
+                            name="title"
+                            value={this.state.title}
                             onChange={this.handleChange}
-                            rows="5"
                         />
-                    </div>
-                </form>
-                <div className="form-group">
-                        <label htmlFor="contact">Contact Me</label>
-                        <input 
-                        type="text" 
-                        name="contact" 
+                        <MDBInput
+                            label="Your Name"
+                            icon="user-edit"
+                            group
+                            type="text"
+                            validate
+                            error="wrong"
+                            success="right"
+                            name="authorName"
+                            value={this.state.authorName}
+                            onChange={this.handleChange}
+                        />
+                    </form>
+                    <MDBInput
+                        label="Contact Me (optional)"
+                        icon="id-card"
+                        group
+                        type="text"
+                        name="contact"
                         value={this.state.contact}
                         onChange={this.handleChange}
-                        className="form-control form-control-lg" />
-                    </div>
-                <div className="form-group">
-                        <label htmlFor="date">Date</label>
-                        <input 
-                        type="text" 
-                        name="date" 
+                    />
+                    <MDBInput
+                        label="Date"
+                        icon="calendar-alt"
+                        group
+                        type="text"
+                        name="date"
                         value={this.state.date}
                         onChange={this.handleChange}
-                        className="form-control form-control-lg" />
-                    </div>
-                    <button onClick={this.handleSubmit}>Post</button>
+                    />
+                    <form>
+                        <MDBInput
+                            type="textarea"
+                            rows="5"
+                            label="Your Post"
+                            icon="pencil-alt"
+                            name="content"
+                            value={this.state.content}
+                            onChange={this.handleChange}
+                        />
+                    </form>
+                    <Fragment>
+                        <MDBBtn onClick={this.handleSubmit} color="default">
+                            POST <MDBIcon far icon="paper-plane" />
+                        </MDBBtn>
+                    </Fragment>
+                </MDBContainer>
             </div>
         );
     }
