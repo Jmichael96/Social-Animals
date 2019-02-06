604b887e14482b3f9372cdfe973aa6af006309d8
Jeffrey196 <jeffreyvh@ymail.com>
2019-02-06 15:07:40 -0600

    update

--- client/src/components/Carousel/index.js
+++ client/src/components/Carousel/index.js
@@ -13,7 +13,7 @@ const CarouselPage = () => {
                     <MDBCarousel id="object" activeItem={1} length={4} showControls={true} showIndicators={true} className="z-depth-5">
                         <MDBCarouselInner>
                             <MDBCarouselItem itemId="1">
-                                <img id="img" className="d-block w-100" src={/img6.jpg} alt="First slide" />
+                                <img id="img" className="d-block w-100" src="./img6.jpg" alt="First slide" />
                             </MDBCarouselItem>
                             <MDBCarouselItem itemId="2">
                                 <img id="img" className="d-block w-100" src="/assets/images/img8.jpg" alt="Second slide" />
@@ -28,7 +28,6 @@ const CarouselPage = () => {
                     </MDBCarousel>
                 </MDBCol>
                 <MDBCol md="4"></MDBCol>
-
             </MDBRow>
         </MDBContainer>
     );
